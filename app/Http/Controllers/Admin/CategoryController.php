9ec770ee549f06f0c0808a12b9754ecd53468fd2
avkar33 <kmi3342@protonmail.com>
2021-03-07 19:06:58 +0300

    add category delete

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -93,6 +93,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->route('admin.category.index');
     }
 }
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -11,8 +11,9 @@
 
         <hr>
 
-        <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fafa-plus-square-0">Создать категорию</i></a>
-        <table  class="table table-striped">
+        <a href="{{ route('admin.category.create') }}" class="btn btn-primary pull-right"><i
+                class="fafa-plus-square-0">Создать категорию</i></a>
+        <table class="table table-striped">
             <thead>
                 <th>Наименование</th>
                 <th>Публикация</th>
@@ -21,15 +22,24 @@
             <tbody>
                 @forelse ($categories as $category)
                     <tr>
-                        <td>{{$category->title}}</td>
-                        <td>{{$category->published}}</td>
-                        <td>
-                            <a href="{{route("admin.category.edit", $category)}}"><i class="fa fa-edit"></i></a>
+                        <td>{{ $category->title }}</td>
+                        <td>{{ $category->published }}</td>
+                        <td class="text-right">
+                            <form onsubmit="if(confirm('Удалить?')){return true}else{return false}"
+                                action="{{ route('admin.category.destroy', $category) }}" method="post">
+                                <input type="hidden" name="_method" value="DELETE">
+                                {{ csrf_field() }}
+                                <a class ="btn btn-default" href="{{ route('admin.category.edit', $category) }}"><i class="fa fa-edit"></i></a>
+
+                                <button type="submit" class="btn"><i class="fa fa-trash-o"></i></button>
+                            </form>
                         </td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan=3 class="text-center"><h2>Данные отсутствуют</h2></td>
+                        <td colspan=3 class="text-center">
+                            <h2>Данные отсутствуют</h2>
+                        </td>
                     </tr>
                 @endforelse
             </tbody>
@@ -37,7 +47,7 @@
                 <tr>
                     <td colspan=3>
                         <ul class="pagination pull-right">
-                            {{$categories->links()}}
+                            {{ $categories->links() }}
                         </ul>
                     </td>
                 </tr>
